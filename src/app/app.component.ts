1db7dcd54e054ecb99efcda66384bb3cade25a83
Sebastian Santis <sebastian.santis@sofka.com.co>
2025-01-31 09:58:20 -0500

    Finalizando primer vistazo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface Item {
+  id: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mision-life';
+  
+  sendWhatsappMessage(text: string): void {
+    window.open(`https://wa.me/3106589572?text=${text.replace(/ /g, '%20')}`, '_blank');
+  }
+
+  openNewTab(link: string):void{
+    window.open(link, '_blank');
+  }
+
 }
